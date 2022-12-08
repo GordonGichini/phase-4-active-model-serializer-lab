8396001e0fa6951238ffc07f7ba3c44ddffa320f
GordonGichini <gordongichini1@gmail.com>
2022-12-08 20:43:38 +0300

    commit tree 3cd810747528f90654e0027cef254e7e734fe3d8

---
+++ app/serializers/author_post_serializer_serializer.rb
@@ -0,0 +1,8 @@
+class AuthorPostSerializerSerializer < ActiveModel::Serializer
+  attributes :title, :short_content
+
+  has_many :tags, serializer: AuthorPostTagSerializer 
+  def short_content
+    "#{self.object.content[0...40]}..."
+  end
+end
